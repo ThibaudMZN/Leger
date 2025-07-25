1030e4f7d2bccbcf6b0ead4f1e98acca4752ab41
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-07-26 00:27:44 +0200

    Fixing prettier

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -1,3 +1,20 @@
-<section>
+<script lang="ts">
+    export let columns: number = 1;
+</script>
+
+<section class="col-{columns}">
     <slot />
 </section>
+
+<style lang="scss">
+    section {
+      display: grid;
+      align-items: center;
+      justify-items: center;
+      gap: 1rem;
+
+      &.col-3 {
+        grid-template-columns: repeat(3, 1fr);
+      }
+    }
+</style>
--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -1,9 +1,29 @@
-<p>
+<script lang="ts">
+    export let size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
+</script>
+
+<p class={size}>
     <slot />
 </p>
 
-<style>
+<style lang="scss">
     p {
-        color: red;
+      font-family: 'Helvetica', 'Arial', sans-serif;
+
+      &.sm {
+        font-size: 0.75rem;
+      }
+
+      &.md {
+        font-size: 1rem;
+      }
+
+      &.lg {
+        font-size: 2rem;
+      }
+
+      &.xl {
+        font-size: 3rem;
+      }
     }
 </style>
