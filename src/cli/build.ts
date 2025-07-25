8bdacac5a875cd38452f6d2a75f296279119ec6c
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-07-25 09:20:40 +0200

    Adding naive svelte builder

---
+++ src/cli/build.ts
@@ -0,0 +1,37 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { parse } from '$parser/parser';
+import { render } from '$renderer/renderer';
+
+export type BuildOptions = {
+    paths: {
+        input: string;
+        output: string;
+    }
+};
+
+const defaultOptions: BuildOptions = {
+    paths: {
+        input: 'pages',
+        output: '.slim/generated',
+    }
+}
+
+export async function build(options: BuildOptions = defaultOptions): Promise<void> {
+    const inDir = path.resolve(options.paths.input);
+    const outDir = path.resolve(options.paths.output);
+    await fs.mkdir(outDir, { recursive: true });
+    const files = await fs.readdir(inDir);
+
+    for (const file of files) {
+        if (!file.endsWith('.slim')) continue;
+
+        const name = file.replace('.slim', '');
+        const raw = await fs.readFile(path.join(inDir, file), 'utf-8');
+        const ast = parse(raw);
+        const svelteCode = render(ast);
+
+        const outPath = path.join(outDir, `${name}.svelte`);
+        await fs.writeFile(outPath, svelteCode, 'utf-8');
+    }
+}
---
+++ tests/cli/build.spec.ts
@@ -0,0 +1,91 @@
+import {describe, it, Mock, beforeEach, afterEach} from "node:test";
+import assert from "node:assert";
+import {build, BuildOptions} from "$cli/build";
+
+import path from "path";
+import fs from "fs/promises";
+
+describe("Slim CLI build command", () => {
+    let mockPathResolve: Mock<any>;
+    let mockFsMkdir: Mock<any>;
+    let mockFsReaddir: Mock<any>;
+    let mockFsReadFile: Mock<any>;
+    let mockFsWriteFile: Mock<any>;
+
+    const defaultTestOptions: BuildOptions = {paths: {input: '/input', output: '/output'}};
+
+    beforeEach((context) => {
+        if('mock' in context) {
+            mockPathResolve = context.mock.method(path, 'resolve', (path: string) => path);
+            mockFsMkdir = context.mock.method(fs, 'mkdir', async () => '');
+            mockFsReaddir = context.mock.method(fs, 'readdir', async () => []);
+            mockFsReadFile = context.mock.method(fs, 'readFile', async () => '');
+            mockFsWriteFile = context.mock.method(fs, 'writeFile', async () => {});
+        }
+    });
+
+    afterEach((context) => {
+        if ('mock' in context)
+            context.mock.reset();
+    });
+
+    it("can resolve input and output path", async () => {
+        let resolvedPath: string[] = [];
+        mockPathResolve.mock.mockImplementation((target: string) => {
+            resolvedPath.push(target);
+            return '';
+        })
+
+        await build(defaultTestOptions);
+
+        assert.deepEqual(resolvedPath, ['/input', '/output']);
+    });
+
+    it("can create output dir", async () => {
+        let createdDirectory: Record<string, any> = {};
+        mockFsMkdir.mock.mockImplementation(async (target: string, options: Record<string, any>) => {
+            createdDirectory = {target, options};
+        });
+
+        await build(defaultTestOptions);
+
+        assert.equal(createdDirectory.target, '/output');
+        assert.deepEqual(createdDirectory.options, { recursive: true });
+    });
+
+    it("can read all files from input directory", async () => {
+        let readDirectory: string = '';
+        mockFsReaddir.mock.mockImplementation(async (target: string) => {
+            readDirectory = target;
+            return []
+        });
+
+        await build(defaultTestOptions);
+
+        assert.equal(readDirectory, '/input');
+    });
+
+    it("can generate svelte files", async () => {
+        let generatedFile: Record<string, any> = {};
+        mockFsReaddir.mock.mockImplementation(async () => ['index.slim']);
+        mockFsReadFile.mock.mockImplementation(async () => 'text(size=large) Hello, world!');
+        mockFsWriteFile.mock.mockImplementation(async (target: string, output: string) => {
+            generatedFile = {target, output};
+        });
+
+        await build(defaultTestOptions);
+        assert.match(generatedFile.target, /\/output\/index.svelte/);
+        assert.equal(generatedFile.output, '<Text size="large">Hello, world!</Text>');
+    });
+
+    it('should omit non .slim files', async () => {
+        let generatedFile: Record<string, any> = {};
+        mockFsReaddir.mock.mockImplementation(async () => ['index.not-slim']);
+        mockFsWriteFile.mock.mockImplementation(async (target: string, output: string) => {
+            generatedFile = {target, output};
+        });
+
+        await build(defaultTestOptions);
+        assert.deepEqual(generatedFile, {});
+    });
+});
